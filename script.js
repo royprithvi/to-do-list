af6685b718f1177c559884da8f10805ac0f865e1
royprithvi <152856911+royprithvi@users.noreply.github.com>
2023-12-21 20:24:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+ const a=document.getElementById("input");
+const b=document.getElementById("list-container");
+
+function addTask(){
+  if(a.value===''){
+    alert("Please enter a task");
+  }
+  else{
+    const li=document.createElement("li");
+    li.innerHTML=a.value;
+    b.appendChild(li);
+    let span=document.createElement("span");
+    span.innerHTML="\u00d7";
+    li.appendChild(span);
+  }
+  a.value = '';
+}
+b.addEventListener("click",function(e){
+  if(e.target.tagName==="LI"){
+  e.target.classList.toggle("checked")
+  }
+    else if(e.target.tagName==="SPAN"){
+      e.target.parentElement.remove();
+    }
+},false);
